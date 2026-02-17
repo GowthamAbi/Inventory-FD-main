206cd838b38be97a74871ca1908d27d1eb5e103d
GowthamAbi <gowtham2131ece@gmail.com>
2026-02-18 00:03:20 +0530

    sideBar Compelet

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,55 +1,133 @@
 import { useState } from 'react'
-import Home from '../../Icons/SidebarIcons/home.png'
-import { SidebarIcon } from '../../Icons/SidebarIcons/SidebarIcons'
+import { home, inventory,arrow, sales,items, purchase, reports, documents } from '../../Icons/SidebarIcons/SidebarIcons'
 export default function SideBar() {
-    const[active,setActive]=useState(false)
+    const[inventoryActive,setInventoryActive]=useState(false)
+    const[salesActive,setSalesActive]=useState(false)
+    const[itemsActive,setItemsActive]=useState(false)
+    const[purchaseActive,setPurchaseActive]=useState(false)
+    const[isActive ,setIsActive]=useState(null)
+    const[isSelect ,setIsSelect]=useState(null)
+    
+    
   return (
-    <div className='bg-[#21263c] w-64  h-screen '>
+    <div className='bg-[#21263c] w-64  h-screen overflow-y-auto leading-10 '>
 
         <h1 className='bg-[#101324] text-center text-white text-xl mb-4 py-4 rounded-b-2xl'>UG Inventory</h1>
 
-            <div className="flex items-center gap-2 pl-8 mx-2 pb-4">
-                {SidebarIcon.home }
-                <p className='text-white'>Home</p>
+            <div className="flex items-center   gap-2 pl-8  cursor-pointer  hover:bg-gray-700 rounded-lg">
+                {home.map((item,index)=>(
+                    <div key={index} className="flex items-center gap-2">
+                        
+                            {item.svg}
+                        <p className='text-white'>{item.name}</p>
+                    </div>
+                )) }
+                
             </div>
 
-            <div className=" pl-8 mx-2 pb-4">
-                <div className="flex items-center gap-2">
-                {SidebarIcon.items }
-                <p className='text-white'>Items</p>
-                </div>
-                <ul className=' text-white pl-8 py-2'>
-                    <li className='pb-2'>Items</li>
-                    <li className='pb-2'>Composite Items</li>
-                    <li className='pb-2'>Item Groups</li>
-                    <li className='pb-2'>Price list</li>
-                </ul>
+            <div className="cursor-pointer ">
+               
+                {items.map((item,index)=>(
+                    <div key={index}  >
+                        <div className="flex items-center gap-2 pb-2 hover:bg-gray-700 rounded-lg" onClick={()=>setItemsActive(!itemsActive)} >
+                           {itemsActive ? arrow.up:arrow.down }
+                            {item.svg}
+                            <p className='text-white '>{item.name}</p>
+                        </div>
+                        {itemsActive && item.list.map((list,j)=>(
+                            <ul key={j} className="  text-white" onClick={()=>setIsSelect(list)}>
+                                <li className={`${isSelect===list?"bg-blue-500 pl-14 rounded-lg":"pl-14 hover:bg-gray-700"}`}>{list}</li>
+                            </ul>
+                        ))}
+                    </div>
+                )) }
+                
+           
             </div>
 
-            <div className="flex items-center gap-2 pl-8 mx-2 pb-4">
-                {SidebarIcon.inventory }
-                <p className='text-white'>Inventory</p>
+            <div className="cursor-pointer ">
+               
+                {inventory.map((item,index)=>(
+                    <div key={index}  >
+                        <div className="flex items-center gap-2 pb-2 hover:bg-gray-700 rounded-lg" onClick={()=>setInventoryActive(!inventoryActive)}>
+                           {inventoryActive ? arrow.up:arrow.down }
+                            {item.svg}
+                            <p className='text-white '>{item.name}</p>
+                        </div>
+                        {inventoryActive && item.list.map((list,j)=>(
+                            <ul key={j} className="  text-white" onClick={()=>setIsSelect(list)}>
+                                <li className={`${isSelect===list?"bg-blue-500 pl-14 rounded-lg":"pl-14 hover:bg-gray-700"}`}>{list}</li>
+                            </ul>
+                        ))}
+                    </div>
+                )) }
+                
+           
             </div>
 
-            <div className="flex items-center gap-2 pl-8 mx-2 pb-4">
-                {SidebarIcon.sales }
-                <p className='text-white'>Sales</p>
+            <div className="cursor-pointer">
+               
+                {sales.map((item,index)=>(
+                    <div key={index}  >
+                        <div className="flex items-center gap-2 pb-2 hover:bg-gray-700 rounded-lg" onClick={()=>setSalesActive(!salesActive)}>
+                           {salesActive ? arrow.up:arrow.down }
+                            {item.svg}
+                            <p className='text-white '>{item.name}</p>
+                        </div>
+                        {salesActive && item.list.map((list,j)=>(
+                            <ul key={j} className="  text-white" onClick={()=>setIsSelect(list)}>
+                                <li className={`${isSelect===list?"bg-blue-500 pl-14 rounded-lg":"pl-14 hover:bg-gray-700"}`}>{list}</li>
+                            </ul>
+                        ))}
+                    </div>
+                )) }
+                
+           
             </div>
 
-            <div className="flex items-center gap-2 pl-8 mx-2 pb-4">
-                {SidebarIcon.purchase }
-                <p className='text-white'>Purchases</p>
+            <div className="cursor-pointer ">
+               
+                {purchase.map((item,index)=>(
+                    <div key={index}  >
+                        <div className="flex items-center gap-2 pb-2 hover:bg-gray-700 rounded-lg" onClick={()=>setPurchaseActive(!purchaseActive)}>
+                           {purchaseActive ? arrow.up:arrow.down }
+                            {item.svg}
+                            <p className='text-white '>{item.name}</p>
+                        </div>
+                        {purchaseActive && item.list.map((list,j)=>(
+                            <ul key={j} className=" text-white" onClick={()=>setIsSelect(list)}>
+                                <li className={`${isSelect===list?"bg-blue-500 pl-14 rounded-lg":"pl-14 hover:bg-gray-700"}`}>{list}</li>
+                            </ul>
+                        ))}
+                    </div>
+                )) }
+                
+           
             </div>
 
-            <div className="flex items-center gap-2 pl-8 mx-2 pb-4">
-                {SidebarIcon.reports }
-                <p className='text-white'>Reports</p>
+            <div className="flex items-center gap-2 pl-8 cursor-pointer hover:bg-gray-700 rounded-lg">
+                {reports.map((item,index)=>(
+                    <div key={index} className="flex items-center gap-2">
+                        
+                            {item.svg}
+                        <p className='text-white'>{item.name}</p>
+                    </div>
+                )) }
+                
             </div>
 
-            <div className="flex items-center gap-2 pl-8 mx-2 pb-4">
-                {SidebarIcon.documents }
-                <p className='text-white'>Documents</p>
+            <div className="flex items-center gap-2 pl-8  cursor-pointer hover:bg-gray-700 rounded-lg">
+                {documents.map((item,index)=>(
+                    <div key={index} className="flex items-center gap-2">
+                        
+                            {item.svg}
+                        <p className='text-white'>{item.name}</p>
+                    </div>
+                )) }
+                
             </div>
+
+          
     </div>
   )
 }
