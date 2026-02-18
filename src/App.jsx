e137a516e39e3c51cbf01033f1f45f25bcffcfbc
GowthamAbi <gowtham2131ece@gmail.com>
2026-02-18 13:13:41 +0530

    side

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import React from 'react'
 import Navbar from './components/NavBar/Navbar'
 import { Route, RouterProvider, Routes } from 'react-router-dom'
 import LayOut from './LayOut'
-import SideBar from './components/SideBar/SideBar'
+import AddItems from './components/SideBar/1Items/Items/AddItems'
+import ItemsList from './components/SideBar/1Items/Items/ItemsList'
 
 export default function App() {
   return (
     <div>
+  
       <Routes>
-        <Route path='/' element={<LayOut/>}/>
+      
+        <Route path='/' element={<LayOut/>}>
+
+        {/* Items  */}
+        <Route path ='inventory/items/new' element={<AddItems/>}/>
+        <Route path='inventory/items' element={<ItemsList/>}/>
+        </Route>
       </Routes>
-      <SideBar/>
+      
       </div>
   )
 }
--- src/LayOut.jsx
+++ src/LayOut.jsx
@@ -1,10 +1,16 @@
 import { Outlet } from 'react-router-dom'
-import  Nav from './components/NavBar/Navbar'
+import SideBar from './components/SideBar/SideBar'
+import Navbar from './components/NavBar/Navbar'
 export default function LayOut(){
     return(
-        <div>
-            <Nav/>
-        <Outlet/>
+        <div className='flex  '>
+            <SideBar/>
+             <div className='flex-1'>
+                <Navbar/>
+                <main className=''>
+                    <Outlet/>
+                </main>
+             </div>
         </div>
     )
 }
