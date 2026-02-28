add2941d284c1cd127ea98cbd4399161a0fada45
GowthamAbi <gowtham2131ece@gmail.com>
2026-03-01 00:12:22 +0530

    add

--- src/components/SideBar/1Items/Items/AddItems.jsx
+++ src/components/SideBar/1Items/Items/AddItems.jsx
@@ -47,13 +47,17 @@ export default function AddItems() {
   })
 
   const handleChange=(e)=>{
-    e.preventDefault()
 
     const{name,value,checked,type}=e.target
     setformData((pre)=>({...pre,[name]:type==="checkbox"?checked:value}))
 
     
   }
+
+  const handleNestedChange=(e,section)=>{
+      const{name,value,type,checked}=e.target
+      setformData(pre=>({...pre,[section]:{...pre[section],[name]:type==="checkbox"?checked :value}}))
+ }
 
   const handleSubmit=()=>{
 
@@ -131,7 +135,8 @@ export default function AddItems() {
 
 
               {formData.type === "goods" && <div className='flex gap-2 justify-center items-center'>
-                <input type="checkbox" name='returnable' checked={formData.returnable} onChange={handleChange} /> <p>Returnable Item</p>
+                <input type="checkbox" name='returnable' checked={formData.returnable}
+                 onChange={handleChange} /> <p>Returnable Item</p>
               </div>}
 
 
@@ -158,38 +163,39 @@ export default function AddItems() {
 
               <div className='flex flex-col pl-2 w-2/6 '>
                   <span className=' '>Dimensions</span>
-                  <span className='text-gray-400'>(Length X Width X Height)</span>
+                  <span className='text-gray-400'>(Length X Width X Hight)</span>
+                  {formData.weight.length}
                 </div>
                   
                 <div className='border border-gray-300 rounded-lg w-1/2 h-10'>
-                  <input type="text" className='outline-none text-end p-2 w-26 ' /> 
+                  <input type="text" className='outline-none text-end p-2 w-26 ' 
+                  name='length' value={formData.dimensions.length} onChange={(e)=>handleDimention(e,'dimensions')}/> 
                   <span className='text-gray-400 '>x</span>
-                  <input type="text" className='outline-none text-end p-2 w-26'/> 
+                  <input type="text" className='outline-none text-end p-2 w-26' name='width' value={formData.dimensions.width} onChange={(e)=>handleDimention(e,'dimensions')}/> 
                   <span className='text-gray-400 '>x</span>
-                  <input type="text" className='outline-none text-end p-2 w-26 '/>
-                  <select name="" id="" className='outline-none w-14 p-2 text-center '>
+                  <input type="text" className='outline-none text-end p-2 w-26 ' name='hight' value={formData.dimensions.hight} onChange={(e)=>handleDimention(e,'dimensions')}/>
+                  <select name="unit" value={formData.dimensions.unit} onChange={(e)=>handleDimention(e,'dimensions')} className='outline-none w-14 p-2 text-center '>
                     <option value=""></option>
                     <option value="in">in</option>
                     <option value="cm">cm</option>
                   </select>
                 </div>
-            
                     
               </div>
 
               <div className=' flex gap-4  pb-4'>
                     <span className='w-2/6 pl-2'>Manufactures</span>
-                    <input type="text" className='w-1/2 outline-none border border-gray-300 p-1.5 rounded-lg ' />
+                    <input name='manufacturer' value={formData.manufacturer} onChange={handleChange} type="text" className='w-1/2 outline-none border border-gray-300 p-1.5 rounded-lg ' />
               </div>
 
               <div className=' flex gap-4  pb-4'>
                     <span className='w-2/6 pl-2'>Universal Product Code</span>
-                    <input type="text" className='w-1/2 outline-none border border-gray-300 p-1.5 rounded-lg ' />
+                    <input type="text" name='upc' value={formData.upc} onChange={handleChange} className='w-1/2 outline-none border border-gray-300 p-1.5 rounded-lg ' />
               </div>
 
                 <div className=' flex gap-4  pb-4'>
                     <span className='w-2/6 pl-2'>International Article Number</span>
-                    <input type="text" className='w-1/2 outline-none border border-gray-300 p-1.5 rounded-lg ' />
+                    <input type="text" name='ean' value={formData.ean} onChange={handleChange} className='w-1/2 outline-none border border-gray-300 p-1.5 rounded-lg ' />
               </div>  
                 </div>
 
@@ -200,8 +206,8 @@ export default function AddItems() {
 
                     <div className='w-1/2 outline-none border
                     border-gray-300 p-1.5 rounded-lg '>
-                    <input type="text" className='w-[90%] outline-none ' />
-                    <select name="" id="" >
+                    <input type="text" className='w-[90%] outline-none ' name='weight' value={formData.weight.value} onChange={(e)=>handleDimention(e,'weigth')} />
+                    <select id=""  name='unit' value={formData.weight.unit} onChange={(e)=>handleDimention(e,'weigth')}>
                       <option value="kg">kg</option>
                       <option value="g">g</option>
                       <option value="kg">ib</option>
@@ -212,17 +218,17 @@ export default function AddItems() {
 
               <div className=' flex gap-4  pb-4 '>
                     <span className='w-2/6 pl-2'>Brand</span>
-                    <input type="text" className='w-1/2 outline-none border border-gray-300 p-1.5 rounded-lg ' />
+                    <input type="text" className='w-1/2 outline-none border border-gray-300 p-1.5 rounded-lg ' name='brand' value={formData.brand} onChange={handleChange} />
               </div> 
 
               <div className=' flex gap-4  pb-4 '>
                     <span className='w-2/6 pl-2'>Manufacturing Part Number</span>
-                    <input type="text" className='w-1/2 outline-none border border-gray-300 p-1.5 rounded-lg ' />
+                    <input type="text" className='w-1/2 outline-none border border-gray-300 p-1.5 rounded-lg ' name='mpn' value={formData.mpn} onChange={handleChange} />
               </div> 
 
               <div className=' flex gap-4  pb-4 '>
                     <span className='w-2/6 pl-2'>International Standard Book Number</span>
-                    <input type="text" className='w-1/2 outline-none border border-gray-300 p-1.5 rounded-lg ' />
+                    <input type="text" className='w-1/2 outline-none border border-gray-300 p-1.5 rounded-lg ' name='isbn' value={formData.isbn} onChange={handleChange} />
               </div> 
 
                 </div>
@@ -236,7 +242,7 @@ export default function AddItems() {
             <div className='w-1/2'>
 
             <div className='flex gap-2 justify-center items-center mb-4'>
-              <input type="checkbox" />
+              <input type="checkbox" name='enabled' checked={formData.salesInfo.enabled} onChange={(e)=>handleNestedChange(e,'salesInfo')} />
               <h2>Sales Information</h2>
             </div>
 
@@ -244,20 +250,21 @@ export default function AddItems() {
               <span className='w-2/6 text-red-500'>Selling Price*</span>
               <div className='border border-gray-300 flex rounded-lg w-1/2 mx-2 '>
                 <p className='bg-gray-200 p-1'>INR</p>
-                <input className=' outline-none p-1' type="text" name="" id="" />
+                <input className=' outline-none p-1' type="text" name="price" value={formData.salesInfo.price} onChange={(e)=>handleNestedChange(e,'salesInfo')}  />
               </div>
             </div>
 
             <div className='flex w-full pl-2 mb-4'>
               <span className='w-2/6 text-red-500'>Account*</span>
               <div className='border border-gray-300 flex rounded-lg w-1/2 mx-2 '>
-                <input className=' outline-none p-1' type="text" name="" id="" />
+                <input className=' outline-none p-1' type="text" name="account" value={formData.salesInfo.account} onChange={(e)=>handleNestedChange(e,'salesInfo')} />
               </div>
             </div>
 
             <div className='flex w-full pl-2 mb-4'>
               <span className='w-2/6 '>Description</span>
-                <textarea name="" id="" className='w-1/2 mx-2 rounded-lg border border-gray-300'></textarea>
+                <textarea  className='w-1/2 mx-2 rounded-lg border border-gray-300'
+                 name="description" value={formData.salesInfo.description} onChange={(e)=>handleNestedChange(e,'salesInfo')}></textarea>
        
             </div>
 
@@ -267,7 +274,7 @@ export default function AddItems() {
             <div className='w-1/2'>
 
             <div className='flex gap-2 justify-center items-center mb-4'>
-              <input type="checkbox" />
+              <input type="checkbox" name='enabled' checked={formData.purchaseInfo.enabled} onChange={(e)=>handleNestedChange(e,"purchaseInfo")} />
               <h2>Purchase Information</h2>
             </div>
 
@@ -275,27 +282,27 @@ export default function AddItems() {
               <span className='w-2/6 text-red-500'>Cost Price*</span>
               <div className='border border-gray-300 flex rounded-lg w-1/2 mx-2 '>
                 <p className='bg-gray-200 p-1'>INR</p>
-                <input className=' outline-none p-1' type="text" name="" id="" />
+                <input className=' outline-none p-1' type="text" name='costPrice' value={formData.purchaseInfo.costPrice} onChange={(e)=>handleNestedChange(e,"purchaseInfo")} />
               </div>
             </div>
 
             <div className='flex w-full pl-2 mb-4'>
               <span className='w-2/6 text-red-500'>Account*</span>
               <div className='border border-gray-300 flex rounded-lg w-1/2 mx-2 '>
-                <input className=' outline-none p-1' type="text" name="" id="" />
+                <input className=' outline-none p-1' type="text" name='account' value={formData.purchaseInfo.account} onChange={(e)=>handleNestedChange(e,"purchaseInfo")} />
               </div>
             </div>
 
             <div className='flex w-full pl-2 mb-4'>
               <span className='w-2/6 '>Description</span>
-                <textarea name="" id="" className='w-1/2 mx-2 rounded-lg border border-gray-300'></textarea>
+                <textarea name='description' value={formData.purchaseInfo.description} onChange={(e)=>handleNestedChange(e,"purchaseInfo")} className='w-1/2 mx-2 rounded-lg border border-gray-300'></textarea>
        
             </div>
 
             <div className='flex w-full pl-2 mb-4'>
               <span className='w-2/6 '>Preferred Vendor</span>
               <div className='border border-gray-300 flex rounded-lg w-1/2 mx-2 '>
-                <input className=' outline-none p-1' type="text" name="" id="" />
+                <input className=' outline-none p-1' type="text" name='preferredVendor' value={formData.purchaseInfo.preferredVendor} onChange={(e)=>handleNestedChange(e,"purchaseInfo")}/>
               </div>
             </div>
 
@@ -310,7 +317,7 @@ export default function AddItems() {
             <div className='w-1/2 pl-2'>
 
             <div className='flex gap-4 justify-center items-center mb-4'>
-              <input type="checkbox" />
+              <input type="checkbox"  />
               <div>
               <h2>Track Inventory for this item</h2>
               <span className='text-gray-500'>You cannot enable/disable inventory tracking once you've created transactions for this item</span>
@@ -321,12 +328,13 @@ export default function AddItems() {
 
             <div className=' flex mb-4'>
                <span className='w-2/6 text-red-500'>Inventory Account*</span>
-              <input className='rounded-lg outline-none p-1 border border-gray-300 w-1/2' type="text" name="" id="" />
+              <input className='rounded-lg outline-none p-1 border border-gray-300 w-1/2'
+               type="text" name='inventoryAccount' value={formData.inventory.inventoryAccount} onChange={(e)=>handleNestedChange(e,"purchaseInfo")}  />
             </div>
 
             <div className=' flex'>
                <span className='w-2/6 '>Opening Stock</span>
-              <input className='rounded-lg outline-none p-1 border border-gray-300 w-1/2' type="text" name="" id="" />
+              <input className='rounded-lg outline-none p-1 border border-gray-300 w-1/2' type="text" name='trackInventory' value={formData.inventory.trackInventory} onChange={(e)=>handleNestedChange(e,"inventory")} />
             </div>
 
             </div>
@@ -338,7 +346,7 @@ export default function AddItems() {
 
             <div className=' flex w-1/2  items-center'>
                <span className='w-2/6 '>Opening Stock</span>
-              <input className='rounded-lg outline-none p-1 border h-10 border-gray-300 w-1/2' type="text" name="" id="" />
+              <input className='rounded-lg outline-none p-1 border h-10 border-gray-300 w-1/2' type="text" name='openingStock' value={formData.inventory.openingStock} onChange={(e)=>handleNestedChange(e,"inventory")} />
             </div>
        
 
